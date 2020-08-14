4042e2520cbe0c8fa967608f2c30040471a4fef6
RayZ5566 <a04354@gmail.com>
2020-08-14 16:23:45 +0800

    SQL.py

---
+++ SQL.py
@@ -0,0 +1,126 @@
+from google.cloud import bigquery
+import os
+
+#setting environment
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/user/Desktop/Kaggle/SQL/t-variety-286403-e5a0529c090d.json'
+
+
+client = bigquery.Client()
+#loeading dataset
+dataset_ref = client.dataset('hacker_news', project='bigquery-public-data')
+dataset = client.get_dataset(dataset_ref)
+tables = list(client.list_tables(dataset))
+
+for table in tables:
+    print(table.table_id)
+    
+table_ref = dataset_ref.table('full')
+table = client.get_table(table_ref)
+
+table.schema
+
+client.list_rows(table, max_results=5).to_dataframe()
+
+client.list_rows(table, selected_fields=table.schema[:4], max_results=5).to_dataframe()
+
+
+
+query3 = """
+        SELECT score, title
+        FROM `bigquery-public-data.hacker_news.full`
+        WHERE type = "job" 
+        """
+dry_run_config = bigquery.QueryJobConfig(dry_run = True)
+dry_run_query_job = client.query(query3, job_config=dry_run_config)
+print('this query will process {} bytes'.format(dry_run_query_job.total_bytes_processed))
+
+# Only run the query if it's less than 1 MB
+one_mb = 1000*1000
+safe_config = bigquery.QueryJobConfig(maximum_bytes_billed=one_mb)
+# Set up the query (will only run if it's less than 1 MB)
+safe_query_job = client.query(query3, job_config=safe_config)
+# API request - try to run the query, and return a pandas DataFrame
+#GET an ERROR!!!!
+safe_query_job.to_dataframe()
+
+# Only run the query if it's less than 1 GB
+one_gb = 1000*1000*1000
+safe_config1 = bigquery.QueryJobConfig(maximum_bytes_billed = one_gb)
+# Set up the query (will only run if it's less than 1 GB)
+safe_query_job1 = client.query(query3, job_config=safe_config1)
+# API request - try to run the query, and return a pandas DataFrame
+job_post_scores = safe_query_job1.to_dataframe()
+# Print average score for job posts
+job_post_scores.score.mean()
+
+
+#-----------------------------------------------------#
+#Chicago
+client = bigquery.Client()
+dataset_ref = client.dataset('chicago_crime', project='bigquery-public-data')
+dataset = client.get_dataset(dataset_ref)
+
+##How many tables are in the Chicago Crime dataset?
+tables = list(client.list_tables(dataset))
+for table in tables:
+    print(table.table_id)
+    
+##How many columns in the `crime` table have `TIMESTAMP` data?
+
+table_ref = dataset_ref.table('crime')
+table = client.get_table(table_ref)
+
+table.schema
+
+
+##If you wanted to create a map with a dot at the location of each crime, 
+##what are the names of the two fields you likely need to pull out of the crime 
+##table to plot the crimes on a map?
+client.list_rows(table, max_results=5).to_dataframe()
+client.list_rows(table, selected_fields=table.schema[-3:], max_results = 5).to_dataframe()
+
+
+#-----------------------------------------------#
+#What are all the U.S. cities in the OpenAQ dataset?
+dataset_ref =client.dataset('openaq', project='bigquery-public-data')
+dataset = client.get_dataset(dataset_ref)
+tables = list(client.list_tables(dataset))
+for table in tables:
+    print(table.table_id)
+table_ref = dataset_ref.table('global_air_quality')
+table = client.get_table(table_ref)
+table.schema
+client.list_rows(table, max_results=5).to_dataframe()
+# Query to select all the items from the "city" column where the "country" column is 'US'
+query = """
+        SELECT city
+        FROM `bigquery-public-data.openaq.global_air_quality`
+        WHERE country = 'US'
+        """
+client = bigquery.Client()
+query_job = client.query(query)
+us_cities = query_job.to_dataframe()
+us_cities.city.value_counts().head()
+
+query1 = """
+        SELECT city, country
+        FROM `bigquery-public-data.openaq.global_air_quality`
+        WHERE country = 'US'
+        """
+client = bigquery.Client()
+query_job = client.query(query1)
+us_cities = query_job.to_dataframe()
+us_cities.city.value_counts().head()
+
+
+query2 = """
+        SELECT *
+        FROM `bigquery-public-data.openaq.global_air_quality`
+        WHERE country = 'US'
+        """
+query_job1 = client.query(query2)
+cities = query_job1.to_dataframe()
+cities.city.value_counts().head()
+cities.country.value_counts().head()
+cities.location.value_counts().head()
+
