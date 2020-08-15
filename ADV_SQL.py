3b68a3cda023e290da87992b7ed20e1a0bfe8aa9
RayZ5566 <a04354@gmail.com>
2020-08-15 18:14:22 +0800

    ADV_SQL.py

--- ADV_SQL.py
+++ ADV_SQL.py
@@ -72,3 +72,292 @@ union_result.head()
 # Number of users who posted stories or comments on January 1, 2014
 len(union_result)
 
+#
+# Create a "Client" object
+client = bigquery.Client()
+
+# Construct a reference to the "stackoverflow" dataset
+dataset_ref = client.dataset("stackoverflow", project="bigquery-public-data")
+
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
+
+# Construct a reference to the "posts_questions" table
+table_ref = dataset_ref.table("posts_questions")
+
+# API request - fetch the table
+table = client.get_table(table_ref)
+
+# Preview the first five lines of the table
+client.list_rows(table, max_results=5).to_dataframe()
+
+# Construct a reference to the "posts_answers" table
+table_ref = dataset_ref.table("posts_answers")
+
+# API request - fetch the table
+table = client.get_table(table_ref)
+
+# Preview the first five lines of the table
+client.list_rows(table, max_results=5).to_dataframe()
+
+
+#You're interested in exploring the data to have a better understanding of how long it generally takes for questions to receive answers. 
+#Armed with this knowledge, you plan to use this information to better design the order in which questions are presented to Stack Overflow users.
+
+#With this goal in mind, you write the query below, which focuses on questions asked in January 2018. It returns a table with two columns:
+
+
+
+
+# Your code here
+correct_query = """
+              SELECT q.id AS q_id,
+                  MIN(TIMESTAMP_DIFF(a.creation_date, q.creation_date, SECOND)) as time_to_answer
+              FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                  LEFT JOIN `bigquery-public-data.stackoverflow.posts_answers` AS a
+              ON q.id = a.parent_id
+              WHERE q.creation_date >= '2018-01-01' and q.creation_date < '2018-02-01'
+              GROUP BY q_id
+              ORDER BY time_to_answer
+              """
+
+# Run the query, and return a pandas DataFrame
+correct_result = client.query(correct_query).result().to_dataframe()
+print("Percentage of answered questions: %s%%" % \
+      (sum(correct_result["time_to_answer"].notnull()) / len(correct_result) * 100))
+print("Number of questions:", len(correct_result))
+
+
+
+
+# Is it more common for users to first ask questions or provide answers? 
+#After signing up, how long does it take for users to first interact with the website? 
+#To explore this further, you draft the (partial) query in the code cell below.
+
+
+# Your code here
+q_and_a_query = """
+                SELECT q.owner_user_id AS owner_user_id,
+                    MIN(q.creation_date) AS q_creation_date,
+                    MIN(a.creation_date) AS a_creation_date
+                FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                FULL JOIN  `bigquery-public-data.stackoverflow.posts_answers` AS a
+                ON q.owner_user_id = a.owner_user_id 
+                WHERE q.creation_date >= '2019-01-01' AND q.creation_date < '2019-02-01' 
+                    AND a.creation_date >= '2019-01-01' AND a.creation_date < '2019-02-01'
+                GROUP BY owner_user_id
+                """
+                
+#Now you'll address a more realistic (and complex!) scenario. 
+#To answer this question, you'll need to pull information from three different tables! This syntax very similar to the case when we have to join only two tables. For instance, consider the three tables below.            
+#Write a query that returns the following columns:
+
+#id - the IDs of all users who created Stack Overflow accounts in January 2019 (January 1, 2019, to January 31, 2019, inclusive)
+#q_creation_date - the first time the user posted a question on the site; if the user has never posted a question, the value should be null
+#a_creation_date - the first time the user posted a question on the site; if the user has never posted a question, the value should be null
+
+
+three_tables_query = """
+                SELECT u.id AS id,
+                    MIN(q.creation_date) AS q_creation_date,
+                    MIN(a.creation_date) AS a_creation_date
+                FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                    FULL JOIN  `bigquery-public-data.stackoverflow.posts_answers` AS a
+                        ON q.owner_user_id = a.owner_user_id 
+                    RIGHT JOIN `bigquery-public-data.stackoverflow.users` AS u
+                        ON q.owner_user_id = u.id
+                WHERE u.creation_date >= '2019-01-01' and u.creation_date <'2019-02-01'
+                GROUP BY id
+                """
+
+#In the code cell below, write a query that returns a table with a single column:
+#- `owner_user_id` - the IDs of all users who posted at least one question or answer on January 1, 2019.  Each user ID should appear at most once.
+
+#In the `posts_questions` (and `posts_answers`) tables, you can get the ID of the original poster from the `owner_user_id` column.  Likewise, the date of the original posting can be found in the `creation_date` column.  
+              
+all_users_query = """
+                  SELECT q.owner_user_id
+                  FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
+                  WHERE EXTRACT(DATE FROM q.creation_date) = '2019-01-01'
+                  UNION DISTINCT   
+                  SELECT a.owner_user_id
+                  FROM `bigquery-public-data.stackoverflow.posts_answers` AS a
+                  WHERE EXTRACT(DATE FROM a.creation_date) = '2019-01-01'
+                  """
+#----------------------------------------------------------------------------#              
+####analytic-functions
+
+# Create a "Client" object
+client = bigquery.Client()
+# Construct a reference to the "san_francisco" dataset
+dataset_ref = client.dataset("san_francisco", project="bigquery-public-data")
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
+# Construct a reference to the "bikeshare_trips" table
+table_ref = dataset_ref.table("bikeshare_trips")
+# API request - fetch the table
+table = client.get_table(table_ref)
+# Preview the first five lines of the table
+client.list_rows(table, max_results=5).to_dataframe()
+
+
+#We'll work with the San Francisco Open Data dataset. We begin by reviewing the first several rows of the bikeshare_trips table. 
+#(The corresponding code is hidden, but you can un-hide it by clicking on the "Code" button below.)
+
+# Query to count the (cumulative) number of trips per day
+num_trips_query = """
+                  WITH trips_by_day AS
+                  (
+                  SELECT DATE(start_date) AS trip_date,
+                      COUNT(*) as num_trips
+                  FROM `bigquery-public-data.san_francisco.bikeshare_trips`
+                  WHERE EXTRACT(YEAR FROM start_date) = 2015
+                  GROUP BY trip_date
+                  )
+                  SELECT *,
+                      SUM(num_trips)
+                          OVER(
+                              ORDER BY trip_date
+                              ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+                              ) AS cumulative_trips
+                      FROM trips_by_day
+                  """
+
+num_trips_result = client.query(num_trips_query).result().to_dataframe()
+num_trips_result.head()
+
+
+#The query uses a common table expression (CTE) to first calculate the daily number of trips. Then, we use SUM() as an aggregate function.
+
+#Since there is no PARTITION BY clause, the entire table is treated as a single partition.
+#The ORDER BY clause orders the rows by date, where earlier dates appear first.
+#By setting the window frame clause to ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW, we ensure that all rows up to and including the current date are used to calculate the (cumulative) sum. 
+#(Note: If you read the documentation, you'll see that this is the default behavior, and so the query would return the same result if we left out this window frame clause.)
+#The next query tracks the stations where each bike began (in start_station_id) and ended (in end_station_id) the day on October 25, 2015.
+
+start_end_query = """
+                  SELECT bike_number,
+                      TIME(start_date) AS trip_time,
+                      FIRST_VALUE(start_station_id)
+                          OVER(
+                              PARTITION BY bike_number
+                              ORDER BY start_date
+                              ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                              ) AS first_station_id,
+                      LAST_VALUE(end_station_id)
+                          OVER(
+                              PARTITION BY bike_number
+                              ORDER BY start_date
+                              ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                              ) AS last_statinon_id,
+                      start_station_id,
+                      end_station_id
+                  FROM `bigquery-public-data.san_francisco.bikeshare_trips`
+                  WHERE DATE(start_date) = '2015-10-25'
+                  """
+# Run the query, and return a pandas DataFrame
+start_end_result = client.query(start_end_query).result().to_dataframe()
+start_end_result.tail()
+
+
+
+
+#EXCERCISE
+
+# Create a "Client" object
+client = bigquery.Client()
+
+# Construct a reference to the "chicago_taxi_trips" dataset
+dataset_ref = client.dataset("chicago_taxi_trips", project="bigquery-public-data")
+
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
+
+# Construct a reference to the "taxi_trips" table
+table_ref = dataset_ref.table("taxi_trips")
+
+# API request - fetch the table
+table = client.get_table(table_ref)
+
+# Preview the first five lines of the table
+client.list_rows(table, max_results=5).to_dataframe()
+
+#1) How can you predict the demand for taxis?
+#Say you work for a taxi company, and you're interested in predicting the demand for taxis. Towards this goal, you'd like to create a plot that shows a rolling average of the daily number of taxi trips. Amend the (partial) query below to return a DataFrame with two columns:
+
+#trip_date - contains one entry for each date from January 1, 2016, to December 31, 2017.
+#avg_num_trips - shows the average number of daily trips, calculated over a window including the value for the current date, along with the values for the preceding 15 days and the following 15 days, as long as the days fit within the two-year time frame. 
+#For instance, when calculating the value in this column for January 5, 2016, the window will include the number of trips for the preceding 4 days, the current date, and the following 15 days.
+#This query is partially completed for you, and you need only write the part that calculates the avg_num_trips column. Note that this query uses a common table expression (CTE); if you need to review how to use CTEs, you're encouraged to check out this tutorial in the Intro to SQL micro-course.
+avg_num_trips_query = """
+                      WITH trips_by_day AS
+                      (
+                      SELECT DATE(trip_start_timestamp) AS trip_date,
+                          COUNT(*) as num_trips
+                      FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+                      WHERE trip_start_timestamp >= '2016-01-01' AND trip_start_timestamp < '2018-01-01'
+                      GROUP BY trip_date
+                      ORDER BY trip_date
+                      )
+                      SELECT trip_date,
+                          AVG(num_trips)
+                          OVER (
+                               ORDER BY trip_date
+                               ROWS BETWEEN 15 PRECEDING and 15 FOLLOWING
+                               ) AS avg_num_trips
+                      FROM trips_by_day
+                      """
+avg_num_trips_result = client.query(avg_num_trips_query).result().to_dataframe()
+avg_num_trips_result.head()  
+
+
+
+#2) Can you separate and order trips by community area?
+#The query below returns a DataFrame with three columns from the table: pickup_community_area, trip_start_timestamp, and trip_end_timestamp.
+
+#Amend the query to return an additional column called trip_number which shows the order in which the trips were taken from their respective community areas. 
+#So, the first trip of the day originating from community area 1 should receive a value of 1; the second trip of the day from the same area should receive a value of 2. Likewise, the first trip of the day from community area 2 should receive a value of 1, and so on.
+
+trip_number_query = """
+                    SELECT pickup_community_area,
+                        trip_start_timestamp,
+                        trip_end_timestamp,
+                        RANK()
+                            OVER(
+                            PARTITION BY pickup_community_area
+                            ORDER by trip_start_timestamp
+                                ) as trip_number
+                    FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+                    WHERE DATE(trip_start_timestamp) = '2017-05-01'
+                    """
+trip_number_result = client.query(trip_number_query).result().to_dataframe()
+trip_number_result.head()  
+
+
+#3) How much time elapses between trips?
+#The (partial) query in the code cell below shows, for each trip in the selected time frame, the corresponding taxi_id, trip_start_timestamp, and trip_end_timestamp.
+
+#Your task in this exercise is to edit the query to include an additional prev_break column that shows the length of the break (in minutes) that the driver had before each trip started (this corresponds to the time between trip_start_timestamp of the current trip and trip_end_timestamp of the previous trip). 
+#Partition the calculation by taxi_id, and order the results within each partition by trip_start_timestamp.
+
+#Some sample results are shown below, where all rows correspond to the same driver (or taxi_id). Take the time now to make sure that the values in the prev_break column make sense to you!
+ 
+#Hint: The TIMESTAMP_DIFF() function takes three arguments, where the first (trip_start_timestamp) and the last (MINUTE) are provided for you. This function provides the time difference (in minutes) of the timestamps in the first two arguments. 
+#You need only fill in the second argument, which should use the LAG() function to pull the timestamp corresponding to the end of the previous trip (for the same taxi_id).
+break_time_query = """
+                   SELECT taxi_id,
+                       trip_start_timestamp,
+                       trip_end_timestamp,
+                       TIMESTAMP_DIFF(
+                           trip_start_timestamp, 
+                           LAG(trip_end_timestamp, 1) 
+                               OVER (
+                                    PARTITION BY taxi_id 
+                                    ORDER BY trip_start_timestamp), 
+                           MINUTE) as prev_break
+                   FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+                   WHERE DATE(trip_start_timestamp) = '2017-05-01' 
+                   """
+                   
+break_time_result = client.query(break_time_query).result().to_dataframe()
+break_time_result.head()
